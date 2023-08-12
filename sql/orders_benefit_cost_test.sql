1e286e5c5d788d4cdd395b219c467aa7e8860450
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2023-08-13 10:44:08 +1200

    v0.1.4-dev.2

---
+++ sql/orders_benefit_cost_test.sql
@@ -0,0 +1,11 @@
+select
+    order_id,
+    weight * price - voucher as b_payment, -- benefit: get payment
+    score as b_customer,  -- benefit: maintain customer relationship
+    log(10, weight) as b_scale,  -- benefit: large orders save management/communication cost and offer better prospect
+    weight as c_material,  -- cost: input raw material
+    quality as c_quality, -- cost: require quality control
+    max(7.251 - lg_sum_weight, 0) as c_rarity  -- cost: rare specialization adds difficulty
+from orders o join specialization_rarity_training r join customers c
+on o.parts = r.parts and o.specification = r.specification and c.customer_id = o.customer_id
+where date(created_time) > '2020-09-06'
